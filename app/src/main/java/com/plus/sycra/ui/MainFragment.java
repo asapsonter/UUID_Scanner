eea67d28ad6b66988e461243645d06c8b836122d
Sam <aarga@sycratech.com>
2023-06-29 17:09:05 +0800

    bluetooth request permission/  scanned device function -> untested

--- app/src/main/java/com/plus/sycra/ui/MainFragment.java
+++ app/src/main/java/com/plus/sycra/ui/MainFragment.java
@@ -3,8 +3,12 @@ package com.plus.sycra.ui;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +16,24 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.plus.sycra.R;
 import com.plus.sycra.databinding.FragmentMainBinding;
 
-import no.nordicsemi.android.support.v18.scanner.ScanCallback;
-import no.nordicsemi.android.support.v18.scanner.ScanResult;
+import java.util.List;
+
 
 public class MainFragment extends Fragment {
     //private MainViewModel mViewModel;
     private FragmentMainBinding binding;
     private BluetoothLeScanner scanner;
     private ScanCallback scanCallback;
+
+    private static final int REQUEST_CODE_BLE_PERMISSIONS = 1;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -44,53 +52,90 @@ public class MainFragment extends Fragment {
         binding.discover.setText(R.string.discover_txt);
         binding.grouping.setText(R.string.grouping_txt);
 
-        //Toast message for discover button
-        binding.discover.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create and show a toast message for discover button
-                Toast.makeText(requireContext(), "You clicked discover",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        //Toast message for grouping button
-        binding.grouping.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create and show a toast message for grouping button
-                Toast.makeText(requireContext(), "You clicked grouping",
-                        Toast.LENGTH_SHORT).show();
+        //***** on click method that handles discover button ***/
+        binding.discover.setOnClickListener(view1 -> {
+            // Create and show a toast message for discover button
+            Toast.makeText(requireContext(), "You clicked discover",
+                    Toast.LENGTH_SHORT).show();
 
-
-                // Scan for BLE devices
-                scanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
-                scanCallback = new ScanCallback() {
-                    @Override
-                    public void onScanResult(int callbackType, @NonNull ScanResult result) {
-                        super.onScanResult(callbackType, result);
-                        // Display BLE devices in another fragment using RecyclerView
-                    }
-                };
-                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                scanner.startScan(scanCallback);
+            // Scan for BLE devices
+           // scanForBleDevices();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                requestBlePermissions();
             }
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void requestBlePermissions() {
+        String[] permissions = new String[]{
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT
+        };
+        requestPermissions(permissions, REQUEST_CODE_BLE_PERMISSIONS);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_BLE_PERMISSIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                // Permissions granted, scan for BLE devices
+                scanForBleDevices();
+            } else {
+                // Permissions denied, show a toast message or a dialog to explain why they are needed
+                Toast.makeText(requireContext(), "BLE permissions are required to scan and connect to devices", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void scanForBleDevices() {
+        scanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+        scanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                super.onScanResult(callbackType, result);
+                // Display BLE devices in another fragment using RecyclerView
+
+                Bundle bundle = new Bundle();
+                // permission to check bluetooth connection
+                if (ActivityCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.
+                        PERMISSION_GRANTED) {
+                    return;
+                }
+                bundle.putString("device_name", result.getDevice().getName());
+                bundle.putString("device_address", result.getDevice().getAddress());
+
+                // Print the device name and address to the logcat
+                Log.d("BLE_SCAN", "Scanned device: " + result.getDevice().getName() + " (" + result.getDevice().getAddress() + ")");
+
+            }
+
+            @Override
+            public void onBatchScanResults(List<ScanResult> results) {
+                super.onBatchScanResults(results);
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+                super.onScanFailed(errorCode);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            return;
+        }
+        scanner.startScan(scanCallback);
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+
     }
 
 }
